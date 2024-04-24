4526a7a1171b172e8a692940f553615d8a51de02
nandapratama21 <nanda241power@gmail.com>
2024-04-24 20:55:38 +0700

    Database set

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,15 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
+    id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     id ("org.flywaydb.flyway") version "10.11.0"
 }
 
-
-
-buildscript {
-    dependencies {
-        classpath("org.postgresql:postgresql:42.7.1")
-        classpath("org.flywaydb:flyway-database-postgresql:10.11.0")
-    }
-}
 group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
-}
-
-
-flyway {
-    url = System.getenv("DATABASE_URL") ?: "jdbc:postgresql://ep-crimson-unit-a5tia4mt.us-east-2.aws.neon.tech/TestDatabase?user=neondb_owner&password=2NAZcQODYdX6&sslmode=require"
 }
 
 configurations {
@@ -35,13 +22,12 @@ repositories {
     mavenCentral()
 }
 
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.flywaydb:flyway-database-postgresql:10.11.0")
-    implementation ("org.flywaydb:flyway-core:10.11.0")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.flywaydb:flyway-core")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("org.postgresql:postgresql")
@@ -50,6 +36,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
+
+buildscript {
+    dependencies {
+        classpath("org.postgresql:postgresql:42.7.1")
+        classpath("org.flywaydb:flyway-database-postgresql:10.11.0")
+    }
+}
+
+flyway {
+    url = System.getenv("DATABASE_URL") ?: "jdbc:postgresql://localhost:5432/Adpro?user=postgres&password=nandapratama5925"
+}
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
