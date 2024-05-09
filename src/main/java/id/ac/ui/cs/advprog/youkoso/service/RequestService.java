2a04c321cb6d94bbb7ea1c0861c9f50a1ab3c3ce
nandapratama21 <nanda241power@gmail.com>
2024-05-09 22:44:22 +0700

    [REFACTOR] remove public modifiers from RequestService interface

--- src/main/java/id/ac/ui/cs/advprog/youkoso/service/RequestService.java
+++ src/main/java/id/ac/ui/cs/advprog/youkoso/service/RequestService.java
@@ -3,9 +3,9 @@ package id.ac.ui.cs.advprog.youkoso.service;
 import id.ac.ui.cs.advprog.youkoso.model.Request;
 import java.util.List;
 public interface RequestService {
-    public Request createRequest(Request request);
-    public Request updateRequest(String requestId, Request updatedRequest);
-    public List <Request> findAllRequest();
-    public Request findRequestById(String requestId);
-    public Request deleteRequest(String requestId);
+    Request createRequest(Request request);
+    Request updateRequest(String requestId, Request updatedRequest);
+    List <Request> findAllRequest();
+    Request findRequestById(String requestId);
+    Request deleteRequest(String requestId);
 }
