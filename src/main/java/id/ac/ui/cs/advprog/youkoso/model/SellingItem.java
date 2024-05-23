452642389ea12574d844df41e7d375fb2d93678e
nandapratama21 <nanda241power@gmail.com>
2024-05-23 15:41:27 +0700

    Create the DB and model for SellingItem

---
+++ src/main/java/id/ac/ui/cs/advprog/youkoso/model/SellingItem.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.youkoso.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+@Table(name = "selling_items")
+public class SellingItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id")
+    private Request request;
+
+    @Column(name = "quantity")
+    private int quantity;
+}
